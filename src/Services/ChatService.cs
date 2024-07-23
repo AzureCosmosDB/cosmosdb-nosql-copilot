bd57b0ca5b2edf09ab945e0c6dab77242033198d
Mark Brown <mjbrown@microsoft.com>
2024-07-23 16:19:56 -0700

    fix

--- src/Services/ChatService.cs
+++ src/Services/ChatService.cs
@@ -122,16 +122,16 @@ public class ChatService
 
             //These functions are just for simple completions without RAG Pattern
             //Generate a completion and tokens used with current context window (just user prompts, no vector search results, non RAG Pattern)
-            //(chatMessage.Completion, chatMessage.CompletionTokens) = await _openAiService.GetChatCompletionAsync(sessionId, contextWindow);
+            (chatMessage.Completion, chatMessage.CompletionTokens) = await _openAiService.GetChatCompletionAsync(sessionId, contextWindow);
             //(chatMessage.Completion, chatMessage.CompletionTokens) = await _semanticKernelService.GetChatCompletionAsync(sessionId, contextWindow);
 
             //These functions are for doing RAG Pattern completions
             //Uncomment SearchProductsAsync() and one of the GetRagCompletionAsync() functions to do RAG Pattern completions
             //Perform vector search for products
-            List<Product> products = await _cosmosDbService.SearchProductsAsync(promptVectors, _productMaxResults);
+            //List<Product> products = await _cosmosDbService.SearchProductsAsync(promptVectors, _productMaxResults);
 
             //Generate a completion and tokens used from current context window and vector search results
-            (chatMessage.Completion, chatMessage.CompletionTokens) = await _openAiService.GetRagCompletionAsync(sessionId, contextWindow, products);
+            //(chatMessage.Completion, chatMessage.CompletionTokens) = await _openAiService.GetRagCompletionAsync(sessionId, contextWindow, products);
             //(chatMessage.Completion, chatMessage.CompletionTokens) = await _semanticKernelService.GetRagCompletionAsync(sessionId, contextWindow, products);
 
             //Cache the prompts in the current context window and their vectors with the generated completion
